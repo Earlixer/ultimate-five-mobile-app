e42bdbabb9ecaef2c31a8b337a646e679468fdc2
Earl Groenewald <earlgroenewald@Yonelas-MacBook-Pro.local>
2022-11-19 23:29:42 +0200

    initial commit

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Insert temporary Icon here and centre it!</Text>
       <StatusBar style="auto" />
     </View>
   );
